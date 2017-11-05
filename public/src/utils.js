cabf2bfd05d08d0685669cf1fb028dfa8756f11c
ls0701 <13311570920@163.com>
2017-11-06 00:55:54 +0800

    完成了登录功能(模块化处理)

---
+++ public/src/utils.js
@@ -0,0 +1,17 @@
+
+define(['jquery'], function ($) {
+
+    // 检测登录
+    $.ajax({
+        url: '/api/employee/checkRootLogin',
+        type: 'get',
+        success: function (info) {
+            if(info.error) {
+                location.href = '/login.html';
+            }
+        }
+    })
+
+   
+
+})
